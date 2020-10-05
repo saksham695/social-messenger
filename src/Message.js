7d335ee83e526f95b1370523bd751b495f55e044
saksham695 <saksham695@gmail.com>
2020-10-05 17:09:41 +0530

    added time of message

--- src/Message.js
+++ src/Message.js
@@ -1,9 +1,22 @@
 import { Card, CardContent, Typography } from "@material-ui/core";
 import React, { forwardRef } from "react";
 import "./Message.css";
+import Timestamp from "react-timestamp";
+
+function getTimeFromDate(time) {
+  var timestamp = time * 1000;
+  return new Date(timestamp).toTimeString();
+}
 
 const Message = forwardRef(({ message, userName }, ref) => {
   const isUser = userName === message.userName;
+  const time = getTimeFromDate(message.timestamp).split(" ")[0].split(":");
+
+  const messageTime =
+    time[0] >= 12
+      ? `${time[0] - 12} : ${time[1]} pm`
+      : `${time[0]} : ${time[1]} am`;
+
   return (
     <div
       ref={ref}
@@ -24,6 +37,7 @@ const Message = forwardRef(({ message, userName }, ref) => {
             {`${message.message} `}
           </Typography>
         </CardContent>
+        <p style={{ textAlign: "right", fontSize: "10px" }}>{messageTime}</p>
       </Card>
     </div>
   );
